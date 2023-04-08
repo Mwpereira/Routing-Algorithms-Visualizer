3deec5d63bba7799d86084588e25b8e6d322c33a
Michael Pereira <mwpereira07@gmail.com>
2023-04-08 19:25:16 -0400

    pushing changes

--- src/components/dijkstras-algorithm-canvas.js
+++ src/components/dijkstras-algorithm-canvas.js
@@ -1,12 +1,18 @@
-import { useState, useEffect, useRef } from "react";
+import {useState, useEffect, useRef, useCallback} from "react";
 import $ from "jquery";
 import "jquery-ui/ui/widgets/draggable";
-import '../assets/styles/dijstras-canvas.css'
+import "../assets/styles/dijstras-canvas.css";
+
+function getSelectedValue() {
+    return $('input[name="toggleAction"]:checked').val();
+}
 
 function DijkstrasAlgorithmCanvas() {
     const containerRef = useRef(null);
     const draggableRef = useRef(null);
-    const [count, setCount] = useState(0);
+    const [letters, setLetters] = useState("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    const [usedLetters, setUsedLetters] = useState([]);
+    const [selectedNodes, setSelectedNodes] = useState([]);
 
     useEffect(() => {
         if (containerRef.current && draggableRef.current) {
@@ -16,37 +22,122 @@ function DijkstrasAlgorithmCanvas() {
         }
     }, [containerRef, draggableRef]);
 
-    const handleAddNodeClick = () => {
-        setCount(count + 1);
+    // Gets the next available letter to use for a node
+    const getNextLetter = () => {
+        for (let i = 0; i < letters.length; i++) {
+            const letter = letters.charAt(i);
+            if (!usedLetters.includes(letter)) {
+                setUsedLetters((prevLetters) => [...prevLetters, letter]);
+                return letter;
+            }
+        }
+        return "";
+    };
 
+    // Handles the drag event for a node (updates the drawing of an edge if effected) -- ACTUALLY NO
+    const handleNodeDrag = (event, ui) => {
+        const node = ui.helper;
+        const position = node.position();
+
+        // Only log the final position when dragging is complete
+        if (event.type === "dragstop") {
+            console.log(`Node ${node.text()} final position: ${position.top}, ${position.left}`);
+        }
+    };
+
+    const handleNodeClick = useCallback((selectedAction, selectedNodes) => {
+        if (selectedAction) {
+            if (selectedAction === "connectNodes") {
+                if (selectedNodes.length === 2) {
+                    // Draw line between nodes
+                    const start = $(`#${selectedNodes[0]}`);
+                    const end = $(`#${selectedNodes[1]}`);
+
+                    const startOffset = start.offset();
+                    const endOffset = end.offset();
+
+                    const canvas = $("<canvas>").appendTo(containerRef.current);
+                    const ctx = canvas[0].getContext("2d");
+                    ctx.strokeStyle = "black";
+                    ctx.lineWidth = 2;
+                    ctx.beginPath();
+                    ctx.moveTo(startOffset.left - containerRef.current.offsetLeft + 25, startOffset.top - containerRef.current.offsetTop + 25);
+                    ctx.lineTo(endOffset.left - containerRef.current.offsetLeft + 25, endOffset.top - containerRef.current.offsetTop + 25);
+                    ctx.stroke();
+
+                    setSelectedNodes([]);
+                } else {
+                    setSelectedNodes([...selectedNodes, selectedNodes]);
+                }
+            } else {
+                // Delete node/edge
+                $(`#${selectedNodes}`).remove(); // Remove selected node
+                setUsedLetters((prevLetters) => prevLetters.filter((letter) => letter !== selectedNodes)); // Put the letter back to the alphabet array
+                setSelectedNodes([]);
+            }
+        }
+    }, [selectedNodes]);
+
+    const handleAddNodeClick = () => {
+        const letter = getNextLetter()
         const newElement = $("<div>")
             .addClass("draggable-component")
             .addClass("circle")
+            .attr("id", letter)
             .css({
                 position: "absolute",
                 top: `50px`,
-                left: `${(count + 1) * 100}px`,
+                left: `100px`,
             })
-            .text(`${count}`);
+            .text(`${letter}`)
+            .click(() => handleNodeClick(getSelectedValue(), letter));
 
         $(draggableRef.current).append(newElement);
 
         $(newElement).draggable({
             containment: containerRef.current,
+            stop: handleNodeDrag,
         });
     };
 
     return (
-        <div className={'is-flex is-flex-direction-column'}  style={{ width: "100%"}}>
-            <section className={'box'} ref={containerRef} style={{ height: 600}}>
-                <div ref={draggableRef} style={{ width: 50, height: 50 }}>
-                    {/* Your draggable component content here */}
+        <div className={'is-flex is-flex-direction-column'} style={{width: "100%"}}>
+            <section className={'box'} ref={containerRef} style={{height: 600}}>
+                <div ref={draggableRef} style={{width: 50, height: 50}}>
+                    {/* draggable component content here */}
                 </div>
             </section>
-            <section className={'my-5 buttons is-grouped'}>
-                <button className={'button'} onClick={handleAddNodeClick}>Add Node</button>
-                <button className={'button'} onClick={handleAddNodeClick}>Add Edge</button>
-                <button className={'button'} onClick={handleAddNodeClick}>Add Weight</button>
+            <section className={'my-5'}>
+                <label className={'label'}>Actions:</label>
+                <div className={'is-flex is-justify-content-space-between'}>
+                    <div className={'buttons is-grouped'}>
+                        <button className={'button'} onClick={handleAddNodeClick}>Add Node</button>
+                    </div>
+
+                    <div className={'control'}>
+                        <label className="radio mr-5">
+                            <input
+                                defaultChecked={true}
+                                className={'mr-2'}
+                                type="radio"
+                                name="toggleAction"
+                                value="connectNodes"
+                            />
+                            Connect Nodes (Draw Edge)
+                        </label>
+                        <label className="radio">
+                            <input
+                                className={'mr-2'}
+                                type="radio"
+                                name="toggleAction"
+                                value="deleteNodeEdge"
+                            />
+                            Delete Node/Edge
+                        </label>
+                    </div>
+
+                    <button className={'button is-primary'} onClick={handleAddNodeClick}>Calculate</button>
+                </div>
             </section>
         </div>
     );
